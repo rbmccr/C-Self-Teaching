f6c71c782d213b31ae9b35a8ad995a7295347f78
Brendan McCray <rbmccr@gmail.com>
2019-04-01 17:07:17 -0500

    added shortcuts file for key shortcuts and completed first two videos in The Console section

---
+++ TheConsole/01_WriteAndWriteLine/EntryPoint.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // puts the cursor on the same line
+        Console.Write("I am learning from the best C# course.");
+        Console.Write("I am learning from the ");
+        Console.Write("best C# course.");
+        // use \n to move to the next line
+        Console.WriteLine("Test the line break \n");
+
+        // prints results on individual lines
+        Console.WriteLine("I'm on my own line");
+        Console.WriteLine("I'm on my own line");
+    }
+}
---
+++ TheConsole/02_OutputSpecialCharsToconsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string exeFile = "Word.exe";
+        // must use escape \ to include special characters
+        string pathToExe = "\"C:\\Program Files\\Microsoft Word\\\"";
+        // can also use @ symbol in front of string with no escape \,
+        // but with quotation marks to escape themselves
+        string pathToExe2 = @"""C:\Program Files\Microsoft Word\""";
+        Console.WriteLine(pathToExe);
+        Console.WriteLine(pathToExe2);
+        // to use interpolation and include the {}, escape with {{}}
+        Console.WriteLine($"{pathToExe}{exeFile}");
+    }
+}
