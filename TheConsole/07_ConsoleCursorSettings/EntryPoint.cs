2ee1d3f08e309fdc49b5a56ce7eab6ed52583bd8
Brendan McCray <rbmccr@gmail.com>
2019-04-03 11:13:05 -0500

    completed console section, moving on to operators and mathematic expressions

---
+++ TheConsole/07_ConsoleCursorSettings/EntryPoint.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class EntryPoint
+{
+    static void Main()
+    {
+        Console.Title = "My App"; // upper left title of console window
+        Console.CursorVisible = true; // false for invisible
+        Console.CursorSize = 100; // height of cursor within row
+        Console.SetCursorPosition(50, 20); // space above and left of cursor
+    }
+}
---
+++ TheConsole/08_ConsoleWindowSize/EntryPoint.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class EntryPoint
+{
+    static void Main()
+    {
+        // control console window size
+        Console.WindowHeight = 40;
+        Console.WindowWidth = 50;
+        Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
+
+        // control scrollable area beyond dimensions of console
+        // must be >= window size
+        // equaling the window size removes scroll bars
+        Console.BufferHeight = 200;
+        Console.BufferWidth = 2000;
+        //Console.SetBufferSize(40, 55);
+
+        // control starting position of scroll
+        // must be smaller than (buffer - window size)
+        Console.WindowTop = 10;
+        Console.WindowLeft = 10;
+        //Console.SetWindowPosition(10, 10);
+    }
+}
